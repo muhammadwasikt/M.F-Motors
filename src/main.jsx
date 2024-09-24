2092315de00cb77170240952bf25d111158bb6a5
muhammadwasikt <wasikhatri11@gmail.com>
2024-09-24 10:11:35 +0500

    animation-install

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import 'animate.css';
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
